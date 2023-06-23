04427a4dd690979d54ffb9b55a7d6b6e965742c8
Urs Schmidt <urs.schmidt@ts.berlin>
2023-06-23 22:17:28 +0200

    🤡 Update CMS config

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,6 +1,16 @@
-import { faTemperatureHigh } from "@fortawesome/free-solid-svg-icons";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
+import { heart, smile } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+
+const getIcon = (icon: string) => {
+  switch (icon) {
+    case "faTemperatureHigh":
+      return faTemperatureHigh;
+    default:
+      return faTemperatureHigh;
+  }
+};
 
 interface HeaderProps {
   title: string;
