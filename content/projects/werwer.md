417a698408b163791c7ed32159dcaf99c2942b68
Urs Schmidt <urs.schmidt@ts.berlin>
2023-06-24 20:14:27 +0200

    ❤️‍🔥 remove buggy pages

--- content/projects/werwer.md
+++
@@ -1,4 +0,0 @@
----
-title: werwer
-subtitle: werwerwer
----
--- next.config.js
+++ next.config.js
@@ -1,17 +1,3 @@
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/,
-  //TODO: add md as well
-  options: {
-    // If you use remark-gfm, you'll need to use next.config.mjs
-    // as the package is ESM only
-    // https://github.com/remarkjs/remark-gfm#install
-    remarkPlugins: [],
-    rehypePlugins: [],
-    // If you use `MDXProvider`, uncomment the following line.
-    // providerImportSource: "@mdx-js/react",
-  },
-});
-
 module.exports = {
   webpack: (cfg) => {
     cfg.module.rules.push({
@@ -22,6 +8,8 @@ module.exports = {
     return cfg;
   },
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-  // Optionally, add any other Next.js config below
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
 };
